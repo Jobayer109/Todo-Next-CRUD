83d0c40ece7af5f5983270064dbf74e3d3e90eea
user.email <jobayer.ahmed109@gmail.com>
2024-02-23 18:06:17 +0600

    Displayed all topics by fetching from DB

--- components/taskList.jsx
+++ components/taskList.jsx
@@ -2,21 +2,41 @@ import Link from "next/link";
 import { FaEdit } from "react-icons/fa";
 import { HiTrash } from "react-icons/hi";
 
-function TaskList() {
+const getTasks = async () => {
+  const res = await fetch("http://localhost:3000/api/topics", {
+    cache: "no-cache",
+  });
+
+  if (!res.ok) {
+    throw new Error("Error occurred in fetching tasks");
+  }
+  return res.json();
+};
+
+const TaskList = async () => {
+  const tasks = await getTasks();
+
   return (
-    <main className="flex items-center justify-between border border-black rounded-md my-3 p-3">
-      <div>
-        <h2 className="text-xl font-bold">HTML</h2>
-        <p>A markup language</p>
-      </div>
-      <div className="flex gap-4">
-        <HiTrash className="text-red-500" size={24} />
-        <Link href={`/editTask/@${123}`}>
-          <FaEdit size={24} />
-        </Link>
-      </div>
+    <main className="  ">
+      {tasks.map((t) => (
+        <div
+          key={t._id}
+          className="flex items-center justify-between border border-black rounded-md my-3 p-3"
+        >
+          <div>
+            <h2 className="text-xl font-bold ">{t.title}</h2>
+            <p>{t.description}</p>
+          </div>
+          <div className="flex gap-4">
+            <HiTrash className="text-red-500" size={24} />
+            <Link href={`/editTask/${t._id}`}>
+              <FaEdit size={24} />
+            </Link>
+          </div>
+        </div>
+      ))}
     </main>
   );
-}
+};
 
 export default TaskList;
